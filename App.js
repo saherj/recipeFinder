dcbfa6527b023c20373ff97644e1e4e058cf3852
Saher Jawaid <saher@Sahers-MacBook-Pro.local>
2019-10-03 11:16:24 -0400

    made pages, node modeules changed, created tabbar

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import{ createBottomTabNavigator } from 'react-navigation-tabs';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import HomeScreen from './home';
+import DiscoverScreen from './discover';
+import ProfileScreen from './profile';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
+const tabNavigator = createBottomTabNavigator({
+  Home: HomeScreen,
+  Discover: DiscoverScreen,
+  Profile: ProfileScreen
+
 });
+
+export default createAppContainer (tabNavigator)
+
+
